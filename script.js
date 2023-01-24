9a93432028cfdedbebe1e0ac7d2f6728037e0baa
Nehuen <nehuensebastianbarolin@gmail.com>
2023-01-24 10:33:37 -0300

    Subida del proyecto final

---
+++ script.js
@@ -0,0 +1,23 @@
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    link.className = "seleccionado";
+
+    //Función que oculta el Menú Responsive (una vez que se ha seleccionado una opción)
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+//Función que muestra el Menú Responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
